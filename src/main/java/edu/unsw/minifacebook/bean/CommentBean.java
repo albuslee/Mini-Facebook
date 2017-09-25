7d158e68443a8333e409f78715580f8a97fac59e
Albus Lee <albuslee@gmail.com>
2017-09-26 01:01:04 +1000

    Delete not finished.

--- src/main/java/edu/unsw/minifacebook/bean/CommentBean.java
+++ src/main/java/edu/unsw/minifacebook/bean/CommentBean.java
@@ -17,48 +17,48 @@ public class CommentBean {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID")
 	private int id;
-	
-	//Comment from USER
-	@ManyToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name = "Reply_from", referencedColumnName="userid",unique=true)
+
+	// Comment from USER
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "Reply_from", referencedColumnName = "userid", unique = true)
 	private UserBean reply_from;
-	
-	//Comment on USER
-	@ManyToOne(cascade=CascadeType.ALL)
-	@JoinColumn(name = "Reply_to", referencedColumnName="userid",unique=true)
+
+	// Comment on USER
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "Reply_to", referencedColumnName = "userid", unique = true)
 	private UserBean reply_to;
-	
+
 	@Column(name = "Content")
 	private String content;
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public void setReplyFrom(UserBean reply_from) {
 		this.reply_from = reply_from;
 	}
-	
+
 	public void setReplyTo(UserBean reply_to) {
 		this.reply_to = reply_to;
 	}
-	
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public UserBean getReplyFrom() {
 		return reply_from;
 	}
-	
+
 	public UserBean getReplyTo() {
 		return reply_to;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
