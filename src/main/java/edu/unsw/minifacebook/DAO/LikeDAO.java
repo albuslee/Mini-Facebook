954c20a5aad32276b3bbc73bb95db89c8aa6e5f7
Albus Lee <albuslee@gmail.com>
2017-09-26 17:14:18 +1000

    Like deletion completed

---
+++ src/main/java/edu/unsw/minifacebook/DAO/LikeDAO.java
@@ -0,0 +1,47 @@
+package edu.unsw.minifacebook.DAO;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.unsw.minifacebook.bean.LikeBean;
+import edu.unsw.minifacebook.bean.UserBean;
+
+public class LikeDAO {
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	private Session getCurrentSession() {
+		return sessionFactory.openSession();
+	}
+	
+	public void saveObject(Object obj) throws HibernateException{
+		this.getCurrentSession().save(obj);
+	}
+	
+	public void addLikes(UserBean like_from, UserBean like_to, int thumb) {
+		LikeBean likeBean = new LikeBean();
+		likeBean.setLikeFrom(like_from);
+		likeBean.setLikeTo(like_to);
+		likeBean.setThumb(thumb);
+		this.getCurrentSession().save(likeBean);
+	}
+	
+	public void deleteLikes(int id) {
+		try {
+			Transaction trn = this.getCurrentSession().beginTransaction();
+			LikeBean unlikeBean = (LikeBean) this.getCurrentSession().get(LikeBean.class, id);
+			this.getCurrentSession().delete(unlikeBean);
+			trn.commit();
+			this.getCurrentSession().close();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			this.getCurrentSession().getTransaction().rollback();
+		}
+
+	}
+
+}
